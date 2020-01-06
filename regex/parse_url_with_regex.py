2f97d0c656a45d26fe12161890a20e454559ee76
dagon1093 <dagon1093@users.noreply.github.com>
2020-01-06 14:05:05 +0300

    Update parse_url_with_regex.py

--- regex/parse_url_with_regex.py
+++ regex/parse_url_with_regex.py
@@ -1,7 +1,9 @@
 import re
 
-url_regex = re.compile(r'(https?)://(www\.[A-Za-z1-9-]{2,256}\.[a-z]{2,6})([-a-zA-Z0-9#$%^&*@:=/+~?]*)')
+#re.VERBOSE | re.I this is compilation flags. VERBOSE ignore tabs or \n, IGNORCASE ignore case of string
+url_regex = re.compile(r'(https?)://(?P<first>www\.[A-Za-z1-9-]{2,256}\.[a-z]{2,6})([-a-zA-Z0-9#$%^&*@:=/+~?]*)',re.VERBOSE | re.I)
+
 match = url_regex.search('dfdgfgfgfgf https://www.udemy.com/course/the-modern-python3-bootcamp/learn/lecture/9328506#overview gfgfgfgfg')
 
-print(match.group())
+print(match.group('first'))
 print(match.groups()) # for all match groups in parenthesses
